1b42ff617ae795977ec5067b446fdd43efe5af0c
Jahongir95 <sjahongir0032@gmail.com>
2020-12-30 21:59:03 +0500

    reo champions carousel structure is created

--- scripts/main.js
+++ scripts/main.js
@@ -58,3 +58,31 @@ var swiper = new Swiper('.swiper-container-advise', {
       },
     }
   });
+//   ********************************************************
+
+var swiper = new Swiper('.swiper-container-champions', {
+    // slidesPerView: 5,
+    // spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    // centeredSlides: true,
+    // init: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 32,
+      },
+    }
+  });
