bc2aa32acf4a0f994535687a5f52e9b36253655e
Jahongir95 <sjahongir0032@gmail.com>
2020-12-30 16:48:34 +0500

    slide made personsively

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,29 @@ var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 30,
+    // slidesPerView: 3,
+    spaceBetween: 25,
     centeredSlides: true,
-  
+
+    
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
     },
+
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        //   spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+        //   spaceBetween: 32,
+        },
+        1088: {
+          slidesPerView: 3,
+        //   spaceBetween: 30,
+        },
+      }
   })
